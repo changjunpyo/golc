9bcf305efecc2e2e6cb3fe871d8daa256d4027c9
JP <changjunpyo@gmail.com>
2021-01-12 18:02:05 +0900

    add Problem2

---
+++ problems/0002.Add-Two-Numbers/0002.add_two_numbers.go
@@ -0,0 +1,33 @@
+package problems
+
+import (
+	"github.com/changjunpyo/golang-leetcode/data-structures"
+)
+
+// Definition for singly-linked list.
+ type ListNode = data_structures.ListNode
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := &ListNode{Val: -1}
+	node := head
+	carry, total := 0, 0
+	for l1 != nil || l2 != nil {
+		if l1 != nil {
+			total += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			total += l2.Val
+			l2 = l2.Next
+		}
+		total += carry
+		carry = total / 10
+		node.Next = &ListNode{Val: total % 10}
+		total = 0
+		node = node.Next
+	}
+	if carry > 0 {
+		node.Next = &ListNode{Val: 1}
+	}
+	return head.Next
+}
---
+++ problems/0002.Add-Two-Numbers/0002.add_two_numbers_test.go
@@ -0,0 +1,48 @@
+package problems
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/changjunpyo/golang-leetcode/data-structures"
+)
+
+type examples2 struct {
+	params2
+	ans2
+}
+
+type params2 struct {
+	nums1 []int
+	nums2 []int
+}
+
+type ans2 struct {
+	ans []int
+}
+
+func Test_Problems2(t *testing.T) {
+
+	ex := []examples2{
+		{
+			params2{[]int{2, 4, 3}, []int{5, 6, 4}},
+			ans2{[]int{7, 0, 8}},
+		}, {
+			params2{[]int{0}, []int{0}},
+			ans2{[]int{0}},
+		},
+		{
+			params2{[]int{9, 9, 9, 9, 9, 9, 9}, []int{9, 9, 9, 9}},
+			ans2{[]int{8, 9, 9, 9, 0, 0, 0, 1}},
+		},
+	}
+	fmt.Println("-------------------- Problem 2. Subsets --------------------")
+	for _, e := range ex {
+		_, p := e.ans2, e.params2
+		fmt.Printf("Input: %v %v\n Output: %v\n", p.nums1, p.nums2,
+			data_structures.ListNodeToIntSlice(
+				addTwoNumbers(data_structures.IntSliceToListNode(p.nums1),
+					data_structures.IntSliceToListNode(p.nums2))))
+	}
+	fmt.Print("\n\n")
+}
