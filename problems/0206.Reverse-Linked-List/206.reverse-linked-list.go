e5e376485a48a199ce294745fb358a0b44ecedfa
JP <changjunpyo@gmail.com>
2021-03-21 22:14:11 +0900

    add recursive solution

--- problems/0206.Reverse-Linked-List/206.reverse-linked-list.go
+++ problems/0206.Reverse-Linked-List/206.reverse-linked-list.go
@@ -1,9 +1,26 @@
 package problems
 
-import "github.com/changjunpyo/golang-leetcode/dataStructures"
+import "github.com/changjunpyo/golc/dataStructures"
 
 // ListNode is linked linked data structure
 type ListNode = dataStructures.ListNode
+
+/*
+func reverseList(head *ListNode) *ListNode {
+    if head == nil{
+        return nil
+    }
+    last := reverseList(head.Next)
+    if last == nil{
+        last = head
+    } else if head.Next != nil{
+        head.Next.Next = head
+    }
+    head.Next = nil
+
+    return last
+}
+*/
 
 func reverseList(head *ListNode) *ListNode {
 	if head == nil {
